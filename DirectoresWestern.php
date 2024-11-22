8b42cfad5f5eba24e2a6f6b143d5f03277af67a1
LemanSachs29 <163987047+LemanSachs29@users.noreply.github.com>
2024-11-22 12:27:20 +0100

    Fin de la actividad

--- DirectoresWestern.php
+++ DirectoresWestern.php
@@ -1,53 +1,62 @@
 <?php
-class Director {
+class Director
+{
     public string $nombre;
     public $peliculas = [];
 
-    public function __construct($nombre, $peliculas = []){
-        $this->nombre=$nombre;
-        $this->peliculas=$peliculas;
+    public function __construct($nombre, $peliculas = [])
+    {
+        $this->nombre = $nombre;
+        $this->peliculas = $peliculas;
     }
 
-    public function agregarPelicula($pelicula){
+    public function agregarPelicula($pelicula)
+    {
         array_push($this->peliculas, $pelicula);
     }
 }
 
-class Pelicula {
+class Pelicula
+{
     public string $titulo;
     public string $duracion;
     public int $anio;
     public $reparto = [];
     public $premios = [];
 
-    public function __construct($titulo, $duracion, $anio, $reparto = [], $premios = []){
+    public function __construct($titulo, $duracion, $anio, $reparto = [], $premios = [])
+    {
         $this->titulo = $titulo;
         $this->duracion = $duracion;
         $this->anio = $anio;
         $this->reparto = $reparto;
         $this->premios = $premios;
     }
-    public function agregarActor($actor){
+    public function agregarActor($actor)
+    {
         array_push($this->reparto, $actor);
     }
-    public function agregarPremio($premio){
+    public function agregarPremio($premio)
+    {
         array_push($this->premios, $premio);
     }
 }
 
-class Actor {
+class Actor
+{
     public string $nombre;
 
-    public function __construct($nombre){
+    public function __construct($nombre)
+    {
         $this->nombre = $nombre;
     }
 }
 
-class Premio {
+class Premio
+{
     public string $nombre;
-    public function __construct($nombre){
+    public function __construct($nombre)
+    {
         $this->nombre = $nombre;
     }
-
 }
-?>
